f6b5fb457a6f466f9e6cc2c4e04261a0f3354af5
aligyoussef <35503547+aligyoussef@users.noreply.github.com>
2018-02-13 15:53:35 -0500

    Update sumText.java

--- sumText.java
+++ sumText.java
@@ -5,12 +5,27 @@ public class sumText
 	public static void main(String[] args)
 	{
 		double sum = 0;
+		int count = 0;
 		try{
 		File list = new File("numlist.txt");
 		Scanner input = new Scanner(list); 
-		while(input.hasNextDouble())
+		while(input.hasNextDouble())   //Getting number of lines
 		{
-			sum = sum + input.nextInt();
+			count++;
+			sum = sum + input.nextDouble();
+		}
+		Scanner input2 = new Scanner(list); 
+			
+		int[] store = new int[count];
+		for(int i = 0; i<store.length; i++)
+		{
+			if(input.hasNextDouble())
+			{
+			store[i] = input.nextDouble();
+			
+			sum = sum + input.nextDouble();
+			}
+			
 		}
 		System.out.println(sum);
 		}
